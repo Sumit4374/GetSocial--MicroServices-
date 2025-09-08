982bd865d2ec206d92ef947a2b628bf2c3900ec1
Sumit4374 <kusumit437@gmail.com>
2025-09-08 23:26:06 +0530

    nothing

--- Services/post_service/src/main/java/com/socialmedia/post_service/Service/PostService.java
+++ Services/post_service/src/main/java/com/socialmedia/post_service/Service/PostService.java
@@ -5,7 +5,6 @@ import java.util.List;
 import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
